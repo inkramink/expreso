afe617d74ad9ef881bc1fb451b9f58527c8500ff
Марина <marinal2005.y.h@ya.ru>
2021-02-15 16:02:21 +0300

    я не знаю

---
+++ hah.py
@@ -0,0 +1,45 @@
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
+from PyQt5.QtCore import Qt, QPointF
+from PyQt5.QtGui import QPainter, QColor, QPolygonF
+from math import radians, cos, sin, sqrt
+
+SCREEN_SIZE = [500, 500]
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('xxx.ui', self)
+        self.btn.clicked.connect(self.paint)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(500, 500, 500, 500)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.x, self.y = randint(0, 500), randint(0, 500)
+        qp.setBrush(QColor(255, 255, 0))
+        r = randint(5, 200)
+        qp.drawEllipse(self.x - r, self.y - r, self.x + r, self.y + r)
+        qp.end()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
